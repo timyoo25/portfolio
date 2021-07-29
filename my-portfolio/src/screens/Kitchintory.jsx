bee6aba2a8424419acc9713772463b6b7b3cfe37
Tim <timyoo25@gmail.com>
2021-07-29 14:45:55 -0600

    kitchintory initialized

--- my-portfolio/src/screens/Kitchintory.jsx
+++ my-portfolio/src/screens/Kitchintory.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 export default function Kitchintory() {
   return (
     <div>
-      <h3>Kitchintory</h3>
+      <div>
+        <h3>Kitchintory</h3>
+      </div>
+      <div>
+        <img src="https://i.imgur.com/LAi4kLY.png" />
+      </div>
+      <div>
+        <div>
+          <h4>Description</h4>
+        </div>
+        <div>
+          <p>Kitchintory is an application for restaurant management. It's a tool to keep track of what's in stock and what is needed so that you minimize waste and maximize profits. This application features full CRUD capabilities on the ingredients with login capabilities. This application was built in a team environment of fellow junior developers. </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <a href="https://kitchintory.netlify.app/">
+            <p>Visit Website</p>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/timyoo25/kitchintory">
+            <p>See Code</p>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
