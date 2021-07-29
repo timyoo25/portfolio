5a398e750a1cec2e767a2a03ea34d41cc63a2ddd
Tim <timyoo25@gmail.com>
2021-07-29 14:37:00 -0600

    healthelite v1

--- my-portfolio/src/screens/HealthElite.jsx
+++ my-portfolio/src/screens/HealthElite.jsx
@@ -4,27 +4,27 @@ export default function HealthElite() {
   return (
     <div>
       <div>
-        <h3>MyFitTrack</h3>
+        <h3>HealthElite</h3>
       </div>
       <div>
-        <img src="https://i.imgur.com/TmGqvy3.png" />
+        <img src="https://i.imgur.com/EQBr0mp.png" />
       </div>
       <div>
         <div>
           <h4>Description</h4>
         </div>
         <div>
-          <p>MyFitTrack is an application with a purpose for users to track their progress and goals. This version allows a user to add and delete custom exercises. This application also splits the exercises into two separate categories: lifts and cardio. Depending on which category the user is recording their exercise, different fields will show for the user to fill out.</p>
+          <p>HealthElite is an application with a purpose for users to have full CRUD capabilities for products and reviews. This application has 5 different categories that users can create products in, and within each product, ability to add a review. </p>
         </div>
       </div>
       <div>
         <div>
-          <a href="https://my-fit-track.netlify.app">
+          <a href="https://healthelite.netlify.app/">
             <p>Visit Website</p>
           </a>
         </div>
         <div>
-          <a href="https://github.com/timyoo25/fitness-tracker">
+          <a href="https://github.com/timyoo25/healthelite">
             <p>See Code</p>
           </a>
         </div>
