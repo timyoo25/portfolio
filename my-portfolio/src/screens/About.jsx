111ac787064fdb5b17fbd9e226715ec7952c7b04
Tim <timyoo25@gmail.com>
2021-07-29 18:02:01 -0600

    about me and contact initialized

--- my-portfolio/src/screens/About.jsx
+++ my-portfolio/src/screens/About.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      <h3>this is about me</h3>
+      <div>
+        <h3>about me</h3>
+      </div>
+      <div>
+        <p>I am a software engineer who who has a background in financial analysis, sales and risk operations. I love to take on new challenges with a mindset of perserverance and humility. With my previous experiences, I've had the opportunities to learn mulitple skillsets and how to work as a cohesive unit amongst peers. As I have not been shy to a fast-paced working environment at Gusto, I've been able to practice adaptation and discipline.</p>
+      </div>
     </div>
   )
 }
--- my-portfolio/src/screens/Contact.jsx
+++ my-portfolio/src/screens/Contact.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-      <h1>contact me</h1>
+      <div>
+        <h1>contact me</h1>
+      </div>
+      <div>
+        <div>
+          <h3>Email:</h3>
+        </div>
+        <div>
+          <h3>timyoo25@gmail.com</h3>
+        </div>
+      </div>
     </div>
   )
 }
