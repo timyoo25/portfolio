45268f53fa3739c66dd60d57bd4d279b9d1714d2
Tim <timyoo25@gmail.com>
2021-08-03 10:13:11 -0600

    updated CSS

--- my-portfolio/src/screens/FitnessTracker.jsx
+++ my-portfolio/src/screens/FitnessTracker.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import "./css/FitnessTracker.css"
 
 export default function FitnessTracker() {
   return (
-    <div>
-      <div>
-        <h3>MyFitTrack</h3>
+    <div className="fitness-parent">
+      <div className="fitness-title-container">
+        <h3 className="fitness-title">MyFitTrack</h3>
       </div>
-      <div>
-        <img src="https://i.imgur.com/TmGqvy3.png" />
-      </div>
-      <div>
-        <div>
-          <h4>Description</h4>
+      <div className="fitness-container">
+        <div className="fitness-image-container">
+          <img className="fitness-image" src="https://i.imgur.com/fc7nHJT.png" />
         </div>
-        <div>
-          <p>MyFitTrack is an application with a purpose for users to track their progress and goals. This version allows a user to add and delete custom exercises. This application also splits the exercises into two separate categories: lifts and cardio. Depending on which category the user is recording their exercise, different fields will show for the user to fill out.</p>
-        </div>
-      </div>
-      <div>
-        <div>
-          <a href="https://my-fit-track.netlify.app">
-            <p>Visit Website</p>
-          </a>
-        </div>
-        <div>
-          <a href="https://github.com/timyoo25/fitness-tracker">
-            <p>See Code</p>
-          </a>
+        <div className="fitness-description-parent">
+          <div className="fitness-description-title-container">
+            <h4 className="fitness-description-title">Description</h4>
+          </div>
+          <div className="fitness-description-container">
+            <p className="fitness-description">MyFitTrack is an application with a purpose for users to track their progress and goals. This version allows a user to add and delete custom exercises. This application also splits the exercises into two separate categories: lifts and cardio. Depending on which category the user is recording their exercise, different fields will show for the user to fill out.</p>
+          </div>
+          <div className="fitness-links-parent">
+            <div className="fitness-website">
+              <a href="https://my-fit-track.netlify.app">
+                <p className="fitness-link">Visit Website</p>
+              </a>
+            </div>
+            <div className="fitness-github">
+              <a href="https://github.com/timyoo25/fitness-tracker">
+                <p className="fitness-link">See Code</p>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- my-portfolio/src/screens/Work.jsx
+++ my-portfolio/src/screens/Work.jsx
@@ -1,19 +1,32 @@
 import { Link } from "react-router-dom"
 
+import "./css/Work.css"
+
 export default function Work() {
   return (
-    <div>
-      <div>
-        <h1>my work</h1>
+    <div className='work-parent'>
+      <div className='work-title-container'>
+        <h1 className='work-title'>my work</h1>
       </div>
-      <div>
-        <Link to="/myfittrack">My Fit Track</Link>
-      </div>
-      <div>
-        <Link to="/kitchintory">Kitchintory</Link>
-      </div>
-      <div>
-        <Link to="/healthelite">HealthElite</Link>
+      <div className='link-container'>
+        <Link className='work-link' id='fittrack' to="/myfittrack">
+          <div className='work-fittrack-container'>
+            <img className="work-image" src="https://i.imgur.com/fc7nHJT.png"/>
+            <h3>My Fit Track</h3>
+          </div>
+        </Link>
+        <Link className='work-link' id='kitchintory ' to="/kitchintory">
+          <div className='work-kitchintory-container'>
+            <img className="work-image" src="https://i.imgur.com/KQrjIap.png" />
+            <h3>Kitchintory</h3>
+          </div>
+        </Link>
+        <Link className='work-link' id='healthelite' to="/healthelite">
+          <div className='work-healthelite-container'>
+            <img className="work-image" src="https://i.imgur.com/I1JleuU.png" />
+            <h3>HealthElite</h3>
+          </div>
+        </Link>
       </div>
     </div>
   )
