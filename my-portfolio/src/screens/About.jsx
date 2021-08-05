f101ba284e0b11b657dae5d64bb46dd531ca4fa7
Tim <timyoo25@gmail.com>
2021-08-05 15:50:00 -0600

    about added

--- my-portfolio/src/screens/About.jsx
+++ my-portfolio/src/screens/About.jsx
@@ -6,12 +6,24 @@ export default function About() {
       <div className='about-image'>
         <img src="https://i.imgur.com/hSMNht0.jpg" />
       </div>
-      <div className='about-container'>
-        <div className='about-title-container'>
-          <h3 className='about-title'>ABOUT ME</h3>
+      <div className="about-fun-container">
+        <div className='about-container'>
+          <div className='about-title-container'>
+            <h3 className='about-title'>ABOUT ME</h3>
+          </div>
+          <div className='summary-container'>
+            <p className='summary'>I am a software engineer who has a background in financial analysis, risk operations and sales. I love to take on new challenges with a mindset of perserverance and humility. With my previous experiences, I've had the opportunities to learn mulitple skillsets and how to work as a cohesive unit amongst peers. As I have not been shy to a fast-paced working environment at Gusto, I've been able to practice adaptation and discipline.</p>
+          </div>
         </div>
-        <div className='summary-container'>
-          <p className='summary'>I am a software engineer who has a background in financial analysis, risk operations and sales. I love to take on new challenges with a mindset of perserverance and humility. With my previous experiences, I've had the opportunities to learn mulitple skillsets and how to work as a cohesive unit amongst peers. As I have not been shy to a fast-paced working environment at Gusto, I've been able to practice adaptation and discipline.</p>
+        <div className="fun-fact-container">
+          <div className="fun-fact-title-container">
+            <h3 className="fun-fact-title">FUN FACTS</h3>
+          </div>
+          <div className="fun-fact-detail-container">
+            <p className="fun-fact-detail">- 1,000 LB club (375 squat, 365 Deadlift, 265 Bench)</p>
+            <p className="fun-fact-detail">- I enjoy detailing cars </p>
+            <p  className="fun-fact-detail">- Lego Enthusiast</p>
+          </div>
         </div>
       </div>
     </div>
